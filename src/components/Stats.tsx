1653d0762957ebcde6409471aee86bd013317bf9
preetym <preetym@mindfiresolutions.com>
2025-12-23 11:11:41 +0530

    husky test

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from "react";
-import BaseStatSinglePokemon from "./BaseStatSinglePokemon"
+import BaseStatSinglePokemon from "./BaseStatSinglePokemon";
+import type { PokemonStat } from "../types/pokemonStat";
 import {
   PieChart,
   Pie,
   Cell,
   Tooltip,
   Legend,
+  Bar,
+  BarChart,
+  XAxis,
+  YAxis,
 } from "recharts";
-import { Bar, BarChart,  XAxis, YAxis   } from 'recharts';
 
 export default function PokemonTypeDistribution() {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<PokemonStat[]>([]);
   const [loading, setLoading] = useState(true);
 
   // Optional: nice colors
   const COLORS = [
-    "#FF5959", "#4E8CFF", "#78C850", "#F5DE50", "#AA5BAA",
-    "#FF9C54", "#9DB7F5", "#A7DB8D", "#FA92B2", "#D1C17D",
-    "#A292BC", "#A38C21", "#EE99AC", "#C03028", "#705848",
-    "#6F35FC", "#98D8D8", "#735797"
+    "#FF5959",
+    "#4E8CFF",
+    "#78C850",
+    "#F5DE50",
+    "#AA5BAA",
+    "#FF9C54",
+    "#9DB7F5",
+    "#A7DB8D",
+    "#FA92B2",
+    "#D1C17D",
+    "#A292BC",
+    "#A38C21",
+    "#EE99AC",
+    "#C03028",
+    "#705848",
+    "#6F35FC",
+    "#98D8D8",
+    "#735797",
   ];
 
   useEffect(() => {
@@ -33,7 +51,7 @@ export default function PokemonTypeDistribution() {
         const pokemonRes = await fetch(p.url);
         const pokemon = await pokemonRes.json();
 
-        pokemon.types.forEach((t: any) => {
+        pokemon.types.forEach((t: PokemonStat) => {
           const typeName = t.type.name;
           typeCount[typeName] = (typeCount[typeName] || 0) + 1;
         });
@@ -42,72 +60,89 @@ export default function PokemonTypeDistribution() {
       // Convert to Recharts format
       const formatted = Object.entries(typeCount).map(([type, count]) => ({
         name: type,
-        value: count
+        value: count,
       }));
 
       setData(formatted);
       setLoading(false);
-      console.log({data})
+      console.log({ data });
     }
 
     loadTypes();
-  }, []);
+  }, [data]);
+
+  const  x=()=>{console.log("hi")}
+
 
   if (loading) return <p>Loading…</p>;
 
   return (
-    
-  <>
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "flex-start",
-      gap: "40px",          // space between charts
-      flexWrap: "wrap"      // allows wrap on small screens
-    }}
-  >
-    <PieChart width={450} height={450} style={{border:"2px solid black", paddingTop: "40px", marginTop: "20px"}}>
-      <Pie
-        dataKey="value"
-        data={data}
-        cx="50%"
-        cy="50%"
-        outerRadius={150}
-        label
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          gap: "40px", // space between charts
+          flexWrap: "wrap", // allows wrap on small screens
+        }}
       >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-      <Legend />
-    </PieChart>
-    <BarChart
-      style={{
-        width: '100%',
-        maxWidth: '800px',
-        maxHeight: '80vh',
-        aspectRatio: 1.618,
-        border:"2px solid black",
-        marginTop: "20px"
-      }}
-      responsive
-      data={data}
-    >
-      <XAxis dataKey="name" />
-      <YAxis
-        type="number"
-        width={100}
-        label={{ value: 'No. of Pokemons', position: 'insideLeft', dx: 0, dy: 20, angle: -90 }}
-      />
-      <Bar dataKey="value" unit="" />
-      <Tooltip />
-    </BarChart>
-  </div>
+        <PieChart
+          width={450}
+          height={450}
+          style={{
+            border: "2px solid black",
+            paddingTop: "40px",
+            marginTop: "20px",
+          }}
+        >
+          <Pie
+            dataKey="value"
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={150}
+            label
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+        <BarChart
+          style={{
+            width: "100%",
+            maxWidth: "800px",
+            maxHeight: "80vh",
+            aspectRatio: 1.618,
+            border: "2px solid black",
+            marginTop: "20px",
+          }}
+          responsive
+          data={data}
+        >
+          <XAxis dataKey="name" />
+          <YAxis
+            type="number"
+            width={100}
+            label={{
+              value: "No. of Pokemons",
+              position: "insideLeft",
+              dx: 0,
+              dy: 20,
+              angle: -90,
+            }}
+          />
+          <Bar dataKey="value" unit="" />
+          <Tooltip />
+        </BarChart>
+      </div>
       <BaseStatSinglePokemon />
-
-  </>
-    
+    </>
   );
 }
